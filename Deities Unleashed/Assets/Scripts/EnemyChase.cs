cb2d6679df8e2626e3def90f24345fcdc48c753a
Feeder1129 <markrevenmacaspac@gmail.com>
2023-11-24 00:14:02 +0800

    commit

--- Deities Unleashed/Assets/Scripts/EnemyChase.cs
+++ Deities Unleashed/Assets/Scripts/EnemyChase.cs
@@ -2,31 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class EnemyChase : MonoBehaviour
 {
-
     public Transform Player;
     [SerializeField] int MoveSpeed = 4;
     [SerializeField] int MaxDist = 10;
     [SerializeField] int MinDist = 5;
-
+    [SerializeField] float ChaseRange = 15f; // New variable for the chasing range
 
     void Update()
     {
-        transform.LookAt(Player);
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
 
-        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        if (distanceToPlayer <= ChaseRange)
         {
+            transform.LookAt(Player);
 
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-
-
-
-            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            if (distanceToPlayer >= MinDist)
             {
-                //Here Call any function U want Like Shoot at here or something
-            }
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
+                if (distanceToPlayer <= MaxDist)
+                {
+                    // Here you can call any function you want, such as shooting
+                }
+            }
         }
     }
 }
