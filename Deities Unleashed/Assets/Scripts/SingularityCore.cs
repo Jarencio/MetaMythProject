ca7a8dc734d33340f32878fa11ba089a1888c313
Feeder1129 <markrevenmacaspac@gmail.com>
2023-12-10 23:50:30 +0800

    cyclops , tiktik, fixed

--- Deities Unleashed/Assets/Scripts/SingularityCore.cs
+++ Deities Unleashed/Assets/Scripts/SingularityCore.cs
@@ -2,7 +2,22 @@
 [RequireComponent(typeof(SphereCollider))]
 public class SingularityCore : MonoBehaviour
 {
-    
+    public CharacterLevelSystem CS;
+
+    void Start()
+    {
+
+        // Find the "Player" GameObject and get the attached "CharacterLevelSystem" script
+        GameObject player = GameObject.Find("Player");
+        if (player != null)
+        {
+            CS = player.GetComponent<CharacterLevelSystem>();
+        }
+        else
+        {
+            Debug.LogError("Player GameObject not found. Make sure the GameObject is named 'Player' and has the 'CharacterLevelSystem' script attached.");
+        }
+    }
     void OnTriggerStay (Collider other) {
         if(other.GetComponent<SingularityPullable>()){
             other.gameObject.SetActive(false);
